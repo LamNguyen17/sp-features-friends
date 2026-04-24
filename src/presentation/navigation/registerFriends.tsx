a28690791c4071e81deb9eafdc7baaf124756727
ForestNguyen <devlamnt176@gmail.com>
2026-04-24 16:15:46 +0700

    add

--- src/presentation/navigation/registerFriends.tsx
+++ src/presentation/navigation/registerFriends.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import type {FriendsDeps} from "presentation/types/injection";
-import {FriendsScreen} from "presentation/screens/FriendsScreen";
+// import {FriendsScreen} from "presentation/screens/FriendsScreen";
+
+// @ts-ignore
+const FriendsScreen = React.lazy(() => import("presentation/screens/FriendsScreen"))
+
 
 export const registerFriends = (Stack: any, deps: FriendsDeps) => {
     return (
         <Stack.Screen name="Friends">
-            {(props: any) => <FriendsScreen {...props} deps={deps}/>}
+            {/*{(props: any) => <FriendsScreen {...props} deps={deps}/>}*/}
+            {(props: any) => (
+                <Suspense fallback={null}>
+                    <FriendsScreen {...props} deps={deps} />
+                </Suspense>
+            )}
         </Stack.Screen>
     )
 }
