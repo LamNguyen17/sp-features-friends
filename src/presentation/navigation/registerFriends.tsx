d7b64f7da579bfd7828b0488b0d6177d39f1e0e3
ForestNguyen <devlamnt176@gmail.com>
2026-04-28 13:34:41 +0700

    v1.0.29

--- src/presentation/navigation/registerFriends.tsx
+++ src/presentation/navigation/registerFriends.tsx
@@ -1,11 +1,11 @@
 import React, { Suspense } from 'react'
 import type {FriendsDeps} from "presentation/types/injection";
-// import {FriendsScreen} from "presentation/screens/FriendsScreen";
+import {FriendsScreen} from "presentation/screens/FriendsScreen";
 
 // @ts-ignore
-const FriendsScreen = React.lazy(() => import(
-    /* webpackChunkName: "sp-features-friends" */
-    "../screens/FriendsScreen"))
+// const FriendsScreen = React.lazy(() => import(
+//     /* webpackChunkName: "sp-features-friends" */
+//     "../screens/FriendsScreen"))
 
 // const FriendsScreen = React.lazy(() =>
 //     /* webpackChunkName: "sp-features-friends" */
@@ -17,12 +17,12 @@ const FriendsScreen = React.lazy(() => import(
 export const registerFriends = (Stack: any, deps: FriendsDeps) => {
     return (
         <Stack.Screen name="Friends">
-            {/*{(props: any) => <FriendsScreen {...props} deps={deps}/>}*/}
-            {(props: any) => (
-                <Suspense fallback={null}>
-                    <FriendsScreen {...props} deps={deps} />
-                </Suspense>
-            )}
+            {(props: any) => <FriendsScreen {...props} deps={deps}/>}
+            {/*{(props: any) => (*/}
+            {/*    <Suspense fallback={null}>*/}
+            {/*        <FriendsScreen {...props} deps={deps} />*/}
+            {/*    </Suspense>*/}
+            {/*)}*/}
         </Stack.Screen>
     )
 }
