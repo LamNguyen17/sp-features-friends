d447296dcf811235e1b9fdb33e1425b5d460fa19
ForestNguyen <devlamnt176@gmail.com>
2026-04-29 14:02:32 +0700

    v1.0.40

--- src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const login: (email: string) => string;
-//# sourceMappingURL=index.d.ts.map
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = void 0;
-// @ts-ignore
-const login = (email) => {
-    return `hello ${email}`;
-};
-exports.login = login;
-//# sourceMappingURL=index.js.map
