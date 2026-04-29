4bad057e73b747f7478da22c58b05eb61b84685b
ForestNguyen <devlamnt176@gmail.com>
2026-04-29 14:41:06 +0700

    v1.0.43

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default as FriendsNavigator } from './presentation/navigation/FriendsNavigator';
+// export { default as FriendsNavigator } from './presentation/navigation/FriendsNavigator';
+
+export const FriendsNavigator = () =>
+    import('./presentation/navigation/FriendsNavigator');
