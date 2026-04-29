36cea115c3af4e321c186f7393be37b04162b892
ForestNguyen <devlamnt176@gmail.com>
2026-04-29 13:11:45 +0700

    v1.0.38

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export { default } from './presentation/navigation/FriendsNavigator'
+// export { default } from './presentation/navigation/FriendsNavigator'
 export * from './presentation/navigation/registerFriends'
 export * from './presentation/store/friendsSlice'
 export * from './presentation/i18n/translations'
 export * from './presentation/types/injection'
 export * from './presentation/routes/routes'
+
+export const loadFriendsNavigator = async () => {
+    const mod = await import(
+        /* webpackChunkName: "sp-features-friends" */
+        './presentation/navigation/FriendsNavigator'
+        );
+
+    return mod.default;
+};
