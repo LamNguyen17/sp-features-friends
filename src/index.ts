48f3cfe57964c3458986eb89a26dfb1eed5b6d6a
ForestNguyen <devlamnt176@gmail.com>
2026-05-25 08:57:09 +0700

    add

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,6 @@ export const friends = {
             './presentation/navigation/FriendsNavigator'
             ),
 };
-
-export const loadFriendsNavigator = () =>
-  import(
-    /* webpackChunkName: "sp-features-friends" */
-    './presentation/navigation/FriendsNavigator'
-    );
-
-// types OK
-export { default as FriendsNavigator } from './presentation/navigation/FriendsNavigator';
 export * from './presentation/types/injection';
 export * from './presentation/store/friendsSlice'
 export * from './presentation/i18n/translations'
